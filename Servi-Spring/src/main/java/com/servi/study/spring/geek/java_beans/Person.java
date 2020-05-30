f27701abd9a32c1a0ee42004c75f27d03be31ee4
高志东 <gaozhdf@yonyou.com>
2020-05-30 14:50:44 +0800

    上传spring demo

---
+++ Servi-Spring/src/main/java/com/servi/study/spring/geek/java_beans/Person.java
@@ -0,0 +1,28 @@
+package com.servi.study.spring.geek.java_beans;
+
+/**
+ * @author servi
+ * @date 2020/5/30
+ */
+public class Person {
+
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
