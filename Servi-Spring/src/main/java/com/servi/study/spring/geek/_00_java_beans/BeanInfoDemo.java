5c3bc4b9b2bfb9f3139fc8c00e03c295baa2517a
高志东 <gaozhdf@yonyou.com>
2020-06-05 21:12:13 +0800

    修改包路径

---
+++ Servi-Spring/src/main/java/com/servi/study/spring/geek/_00_java_beans/BeanInfoDemo.java
@@ -0,0 +1,26 @@
+package com.servi.study.spring.geek._00_java_beans;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.util.stream.Stream;
+
+/**
+ * @author servi
+ * @date 2020/5/30
+ */
+public class BeanInfoDemo {
+
+    public static void main(String[] args) throws IntrospectionException {
+        BeanInfo info = Introspector.getBeanInfo(Person.class,Object.class);
+
+        Stream.of(info.getPropertyDescriptors()).forEachOrdered(p -> {
+            System.out.println(p);
+            System.out.println("=================================");
+            System.out.println(p.getName());
+
+            System.out.println("=================================");
+            System.out.println(p.isHidden());
+        });
+    }
+}
