2c0b047aae34631172c11d75a45b8e9318bab614
高志东 <gaozhdf@yonyou.com>
2020-06-06 13:58:51 +0800

    bean scope 和 bean lifecycle

--- Servi-Spring/src/main/java/com/servi/study/spring/geek/_07_bean_lifecycle/T01_BeanMetadataConfigurationDemo.java
+++ Servi-Spring/src/main/java/com/servi/study/spring/geek/_07_bean_lifecycle/T01_BeanMetadataConfigurationDemo.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.servi.study.spring.geek._07_bean_lifecycle;
 
 import com.servi.study.spring.geek._01_ioc_container_overview.domain.User;
@@ -29,7 +13,7 @@ import org.springframework.core.io.support.EncodedResource;
  * @author servi
  * @since
  */
-public class BeanMetadataConfigurationDemo {
+public class T01_BeanMetadataConfigurationDemo {
 
     public static void main(String[] args) {
         DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
@@ -44,7 +28,11 @@ public class BeanMetadataConfigurationDemo {
         System.out.println("已加载 BeanDefinition 数量：" + beanNumbers);
         // 通过 Bean Id 和类型进行依赖查找
         User user = beanFactory.getBean("user", User.class);
-        System.out.println(user);
+        System.out.println("user->" + user);
+
+        // 通过 Bean Id 和类型进行依赖查找
+        User user1 = (User) beanFactory.getBean("user1");
+        System.out.println("user1->" + user1);
     }
 
 }
