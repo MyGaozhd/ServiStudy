48442f2d6cfa980376fa07f4761e7a8d453cbec0
高志东 <gaozhdf@yonyou.com>
2020-06-18 19:49:20 +0800

    归档

--- Servi-JUC/src/main/java/com/servi/study/juc/_05/T.java
+++ Servi-JUC/src/main/java/com/servi/study/juc/_05/T.java
@@ -5,18 +5,25 @@ package com.servi.study.juc._05;
 
 public class T implements Runnable {
 
-	private /*volatile*/ int count = 100;
-	
-	public /*synchronized*/ void run() { 
-		count--;
-		System.out.println(Thread.currentThread().getName() + " count = " + count);
-	}
-	
-	public static void main(String[] args) {
-		T t = new T();
-		for(int i=0; i<100; i++) {
-			new Thread(t, "THREAD" + i).start();
-		}
-	}
-	
+    private /*volatile*/ int count = 0;
+
+    public /*synchronized*/ void run() {
+        count++;
+        System.out.println(Thread.currentThread().getName() + " count = " + count);
+    }
+
+    public static void main(String[] args) {
+        T t = new T();
+        for (int i = 0; i < 10000; i++) {
+            new Thread(t::run, "THREAD" + i).start();
+        }
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(t.count);
+    }
+
 }
