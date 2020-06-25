ef103628fe4e52659f25364e504b3917bccfcef3
高志东 <gaozhdf@yonyou.com>
2020-06-25 19:30:30 +0800

    spring 国际化

---
+++ Servi-Spring/src/main/java/com/servi/study/spring/geek/_10_i18n/T01_MessageFormatDemo.java
@@ -0,0 +1,50 @@
+package com.servi.study.spring.geek._10_i18n;
+
+import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * {@link MessageFormat} 示例
+ *
+ * @author servi
+ * @see MessageFormat
+ * @since
+ */
+public class T01_MessageFormatDemo {
+
+    public static void main(String[] args) {
+
+        int planet = 7;
+        String event = "a disturbance in the Force";
+
+        String messageFormatPattern = "At {1,time,long} on {1,date,full}, there was {2} on planet {0,number,integer}.";
+        MessageFormat messageFormat = new MessageFormat(messageFormatPattern);
+        String result = messageFormat.format(new Object[]{planet, new Date(), event});
+        System.out.println(result);
+        System.out.println("=============================================");
+
+        // 重置 MessageFormatPattern
+        // applyPattern
+        messageFormatPattern = "This is a text : {0}, {1}, {2}";
+        messageFormat.applyPattern(messageFormatPattern);
+        result = messageFormat.format(new Object[]{"Hello,World", "666"});
+        System.out.println(result);
+        System.out.println("=============================================");
+
+        // 重置 Locale 英文
+        messageFormat.setLocale(Locale.ENGLISH);
+        messageFormatPattern = "At {1,time,long} on {1,date,full}, there was {2} on planet {0,number,integer}.";
+        messageFormat.applyPattern(messageFormatPattern);
+        result = messageFormat.format(new Object[]{planet, new Date(), event});
+        System.out.println(result);
+        System.out.println("=============================================");
+
+        // 重置 Format
+        // 根据参数索引来设置 Pattern
+        messageFormat.setFormat(1,new SimpleDateFormat("YYYY-MM-dd HH:mm:ss"));
+        result = messageFormat.format(new Object[]{planet, new Date(), event});
+        System.out.println(result);
+    }
+}
