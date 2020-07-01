7bf86158a06fb3cdfc0d9fcc52db0a5e44c72a24
高志东 <gaozhdf@yonyou.com>
2020-07-01 21:23:25 +0800

    yaml 配置文件使用

--- Servi-Boot/src/main/java/com/servi/study/boot/controller/C01_Controller.java
+++ Servi-Boot/src/main/java/com/servi/study/boot/controller/C01_Controller.java
@@ -1,7 +1,5 @@
 package com.servi.study.boot.controller;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
