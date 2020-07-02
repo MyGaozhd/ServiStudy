0c94fb54c3eac7e740620b3e92eab88414eab1dc
高志东 <gaozhdf@yonyou.com>
2020-07-02 20:23:08 +0800

    删除无用引用

--- Servi-RPC/src/test/java/com/servi/study/rpc/RPCApplicationTests.java
+++ Servi-RPC/src/test/java/com/servi/study/rpc/RPCApplicationTests.java
@@ -8,10 +8,8 @@ import com.servi.study.rpc.dto.IResponse;
 import com.servi.study.rpc.invoke.IBeforeInvoke;
 import com.servi.study.rpc.invoke.IRPCInvoke;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
 
 import java.util.Collection;
 import java.util.List;
