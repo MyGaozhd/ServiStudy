9e9956b15d01851d1e4b23f194714e971ec54b70
高志东 <gaozhdf@yonyou.com>
2020-07-11 15:59:57 +0800

    增加 rabbitmq 实现demo

--- Servi-MQ/src/test/java/com/servi/study/mq/ServiMqApplicationTests.java
+++ Servi-MQ/src/test/java/com/servi/study/mq/ServiMqApplicationTests.java
@@ -1,13 +1,26 @@
 package com.servi.study.mq;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @SpringBootTest
 class ServiMqApplicationTests {
 
+    @Autowired
+    RabbitTemplate template;
+
     @Test
     void contextLoads() {
+        template.convertAndSend("123");
     }
 
+    @Test
+    void receive() {
+        Message msg = template.receive();
+        System.out.println(msg);
+    }
 }
