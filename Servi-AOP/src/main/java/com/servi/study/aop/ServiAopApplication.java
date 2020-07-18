165650a95b797942492c81e5ff2da10460955fd8
高志东 <gaozhdf@yonyou.com>
2020-07-18 14:03:28 +0800

    增加aop demo project

---
+++ Servi-AOP/src/main/java/com/servi/study/aop/ServiAopApplication.java
@@ -0,0 +1,13 @@
+package com.servi.study.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiAopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiAopApplication.class, args);
+    }
+
+}
