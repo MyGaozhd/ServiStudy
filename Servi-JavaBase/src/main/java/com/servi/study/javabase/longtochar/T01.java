3411bdd6f7dba5a28d75a0058a57563e1effbf89
高志东 <gaozhdf@yonyou.com>
2020-11-16 20:34:50 +0800

    数据库主键设计

---
+++ Servi-JavaBase/src/main/java/com/servi/study/javabase/longtochar/T01.java
@@ -0,0 +1,13 @@
+package com.servi.study.javabase.longtochar;
+
+public class T01 {
+    public static void main(String[] args) {
+
+        Long a = 1L;
+
+
+        String b = Long.toBinaryString(a);
+
+        char s = 1;
+    }
+}
