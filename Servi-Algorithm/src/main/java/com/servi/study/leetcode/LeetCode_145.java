ed7f1235c4d23018fcd257beb4c4962a106e96ec
高志东 <gaozhdf@yonyou.com>
2021-01-18 21:04:42 +0800

    LeetCode

---
+++ Servi-Algorithm/src/main/java/com/servi/study/leetcode/LeetCode_145.java
@@ -0,0 +1,45 @@
+package com.servi.study.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 144. 二叉树的后序遍历
+ */
+public class LeetCode_145 {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        postorderTraversal(root, list);
+        return list;
+    }
+
+    public void postorderTraversal(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+
+        postorderTraversal(root.left, list);
+        postorderTraversal(root.right, list);
+        list.add(root.val);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
---
+++ Servi-Algorithm/src/main/java/com/servi/study/leetcode/LeetCode_94.java
@@ -0,0 +1,44 @@
+package com.servi.study.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+  94. 二叉树的中序遍历
+ */
+public class LeetCode_94 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorderTraversal(root,list);
+        return list;
+    }
+
+    public void inorderTraversal(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, list);
+        list.add(root.val);
+        inorderTraversal(root.right, list);
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
