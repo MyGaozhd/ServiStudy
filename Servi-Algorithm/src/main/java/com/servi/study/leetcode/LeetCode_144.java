7af3889c27800e1e203d42ce731ed5d2c3224fa6
高志东 <gaozhdf@yonyou.com>
2021-01-23 10:12:18 +0800

    leetcode

--- Servi-Algorithm/src/main/java/com/servi/study/leetcode/LeetCode_144.java
+++ Servi-Algorithm/src/main/java/com/servi/study/leetcode/LeetCode_144.java
@@ -1,6 +1,7 @@
 package com.servi.study.leetcode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,13 +9,33 @@ import java.util.List;
  */
 public class LeetCode_144 {
 
-    public List<Integer> preorderTraversal(TreeNode root) {
+    public static void main(String[] args) {
+
+        TreeNode node0 = new TreeNode(0);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node1 = new TreeNode(1, node0, node2);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(3, node1, node4);
+
+        TreeNode node9 = new TreeNode(9);
+        TreeNode node8 = new TreeNode(8, null, node9);
+        TreeNode node6 = new TreeNode(6);
+
+        TreeNode node7 = new TreeNode(7, node6, node8);
+
+        TreeNode node5 = new TreeNode(5, node3, node7);
+
+        System.out.print(Arrays.toString(preorderTraversal(node5).toArray()));
+       
+    }
+
+    public static List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         preorderTraversal(root, list);
         return list;
     }
 
-    public void preorderTraversal(TreeNode root, List<Integer> list) {
+    public static void preorderTraversal(TreeNode root, List<Integer> list) {
         if (root == null) {
             return;
         }
@@ -24,7 +45,7 @@ public class LeetCode_144 {
         preorderTraversal(root.right, list);
     }
 
-    public class TreeNode {
+    public static class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
