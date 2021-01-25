8a4d772d48bfc915bf6c6e26ee643e33eb519deb
高志东 <gaozhdf@yonyou.com>
2021-01-25 15:37:29 +0800

    leetcode

---
+++ Servi-Algorithm/src/main/java/com/servi/study/leetcode/LeetCode_236.java
@@ -0,0 +1,102 @@
+package com.servi.study.leetcode;
+
+/**
+ * 236. 二叉树的最近公共祖先
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ * <p>
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ * <p>
+ * 例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * 输出: 3
+ * 解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+ * 示例 2:
+ * <p>
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * 输出: 5
+ * 解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+ * <p>
+ * <p>
+ * 说明:
+ * <p>
+ * 所有节点的值都是唯一的。
+ * p、q 为不同节点且均存在于给定的二叉树中。
+ */
+public class LeetCode_236 {
+
+    public static void main(String[] args) {
+
+        TreeNode node0 = new TreeNode(0);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node1 = new TreeNode(1, node0, node2);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(3, node1, node4);
+
+        TreeNode node9 = new TreeNode(9);
+        TreeNode node8 = new TreeNode(8, null, node9);
+        TreeNode node6 = new TreeNode(6);
+
+        TreeNode node7 = new TreeNode(7, node6, node8);
+
+        TreeNode node5 = new TreeNode(5, node3, node7);
+
+        System.out.print(new LeetCode_236().lowestCommonAncestor(node5, node1, node3));
+
+    }
+
+    TreeNode ans = null;
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        dfs(root, p, q);
+        return ans;
+    }
+
+    public boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null) {
+            return false;
+        }
+
+        boolean left = dfs(root.left, p, q);
+        boolean right = dfs(root.right, p, q);
+
+        if ((left && right) || ((root.val == p.val || root.val == q.val) && (left || right))) {
+            ans = root;
+        }
+
+        return left || right || root.val == p.val || root.val == q.val;
+    }
+
+    public static class TreeNode {
+
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+
+            val = x;
+        }
+
+        TreeNode(int x, TreeNode left, TreeNode right) {
+
+            this.val = x;
+            this.left = left;
+            this.right = right;
+        }
+
+        @Override
+        public String toString() {
+
+            return val + "";
+        }
+    }
+}
