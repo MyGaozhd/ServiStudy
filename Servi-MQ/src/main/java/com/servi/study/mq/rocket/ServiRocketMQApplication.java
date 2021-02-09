b3e46d2bd6a0eeed33add0231b448fccb156a42f
高志东 <gaozhdf@yonyou.com>
2021-02-09 08:37:24 +0800

    引入RocketMQ

---
+++ Servi-MQ/src/main/java/com/servi/study/mq/rocket/ServiRocketMQApplication.java
@@ -0,0 +1,13 @@
+package com.servi.study.mq.rocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiRocketMQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiRocketMQApplication.class, args);
+    }
+
+}
