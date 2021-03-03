981a53df816eddf49f1a3bd35d94a2a47e0c15de
高志东 <gaozhdf@yonyou.com>
2021-03-03 16:25:55 +0800

    LeetCode

---
+++ Servi-Algorithm/src/main/java/com/servi/study/leetcode/LeetCode_Offer_13.java
@@ -0,0 +1,13 @@
+package com.servi.study.leetcode;
+
+/**
+ * @author servi
+ * @date 2021/3/3
+ */
+public class LeetCode_Offer_13 {
+
+    public int movingCount(int m, int n, int k) {
+
+        return 0;
+    }
+}
